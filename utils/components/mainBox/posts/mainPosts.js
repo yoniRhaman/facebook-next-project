6078eba1cba065fdd1d4809cdd96b2f029e0b381
chaimsolo <145326116+chaimsolo@users.noreply.github.com>
2024-04-30 13:53:56 +0300

    overflow
    overflow added for the posts div

--- utils/components/mainBox/posts/mainPosts.js
+++ utils/components/mainBox/posts/mainPosts.js
@@ -2,14 +2,47 @@ import "./mainPosts.css";
 
 const posts = [
     {
-        title: "",
-        content: "",
+        title: "n cn n rn ",
+        content: "jln j j",
         pictures:[
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
-            ""
         ],
-        postingDate: "",
-        postingTime: "",
+        postingDate: "12-03-2022",
+        postingTime: "15:34",
+        comments: [{
+           name: "",
+           title: "",
+           content: "",
+           picture: "",
+           commentingDate: "",
+           commentingTime: "",
+        }]
+    },
+    {
+        title: "n cn n rn ",
+        content: "jln j j",
+        pictures:[
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        ],
+        postingDate: "12-03-2022",
+        postingTime: "15:34",
+        comments: [{
+           name: "",
+           title: "",
+           content: "",
+           picture: "",
+           commentingDate: "",
+           commentingTime: "",
+        }]
+    },
+    {
+        title: "n cn n rn ",
+        content: "jln j j",
+        pictures:[
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        ],
+        postingDate: "12-03-2022",
+        postingTime: "15:34",
         comments: [{
            name: "",
            title: "",
@@ -24,26 +57,31 @@ const posts = [
 export default function MainPosts(){
     return(
         
-       <div className="column">
-            {posts.map((post, sirialNumber) => (
-                <PostItem  post={post} sirialNumber={sirialNumber} />
+       <div className="column posts-box">
+            {posts.map((post) => (
+                <PostItem  post={post} />
             ))}
        </div>
     );
 }
 
 
-function PostItem(post, sirialNumber){
+function PostItem({post}){
     return (
         <div className="column post-box">
             <h1>{post.title}</h1>
             <hr/>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s"></img>
+            {post.pictures.map((picture)=> (<img src={picture}></img>))}
             <p>{post.content}</p>
+            <p>posted on {post.postingDate} at {post.postingTime}</p>
 
 
             
         </div>
     )
 }
+
+
+
+
 
---
+++ utils/data/freinds.js
@@ -0,0 +1,38 @@
+const freinds = [
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    },
+    {
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfb_-bZ0G28ppp5P7bmwuCUDJC8i6IIe44XLjlj-_fHsLSVoZb3kB2758kmA&s",
+        name: "Melech Constablier"
+    }
+]
