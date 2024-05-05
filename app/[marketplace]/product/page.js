2cad634c279678fc7f25dfb46130ef867cd5fb81
mendy5692 <mmm0533115692@gmail.com>
2024-05-05 14:49:40 +0300

    creat demo product page

--- app/[marketplace]/product/page.js
+++ app/[marketplace]/product/page.js
@@ -1,8 +1,37 @@
+import "./product.css";
 
 function ProductPage() {
   return (
-    <div>Productpage</div>
-  )
+    <div className="my-body row">
+      <div className="imges center">
+        <div className="main-img"></div>
+        <div className="more-imges"></div>
+      </div>
+      <div className="details column">
+        <div className="all-imges">
+          <div className="img1"></div>
+          <div className="img1"></div>
+          <div className="img1"></div>
+          <div className="img1"></div>
+          <div className="img1"></div>
+          <div className="img1"></div>
+        </div>
+        <div className="my-text column">
+          <div className="title">ssss</div>
+          <div className="price">ssss</div>
+          <div className="location">ssss</div>
+          <div className="description">
+            the industry's standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic cently
+            with de
+          </div>
+        </div>
+        <button className="chat">sss</button>
+      </div>
+    </div>
+  );
 }
 
-export default ProductPage
+export default ProductPage;
