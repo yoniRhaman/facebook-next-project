471a887329197114372e831877e8a48b2184486e
yosef-tutian <yoseftutian@gmail.com>
2024-05-05 13:58:38 +0300

    components login

--- utils/components/createPost/createPost.js
+++ utils/components/createPost/createPost.js
@@ -1,66 +1,9 @@
 "use client";
 
-// import { useState } from "react";
-// import "./createPost.css";
-// import { Button } from "@mui/material";
-
-// // Modal component
-// function Modal({ onClose }) {
-//   const [postContent, setPostContent] = useState(""); // State for textarea content
-
-//   const handlePostSubmit = () => {
-//     // Handle post submission (e.g., send data to server)
-//     console.log("Submitted post content:", postContent);
-//     onClose(); // Close the modal
-//   };
-
-//   return (
-//     <div className="modal-container center">
-//       <div className="modal-content">
-//         <textarea
-//           className="input-post"
-//           placeholder="What's on your mind?"
-//           value={postContent}
-//           onChange={(e) => setPostContent(e.target.value)}
-//           maxLength={1000}
-//         />
-//         <Button variant="contained" onClick={handlePostSubmit}>
-//           Next
-//         </Button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// // CreatePost component
-// export default function CreatePost() {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   const handleInputClick = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         // className="input-post"
-//         placeholder="What's on your mind?"
-//         onClick={handleInputClick}
-//       />
-//       {isModalOpen && <Modal onClose={closeModal} />}
-//     </div>
-//   );
-// }
-// CreatePost.js
 import { useState } from "react";
 
 import "./createPost.css"; // Assuming you have a CSS file for styling
-import Modal from "../modal/modal";
+import Modal from "../newPostModal/newPostModal";
 
 export default function CreatePost() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -75,7 +18,8 @@ export default function CreatePost() {
 
   return (
     <div className="post-page-container">
-      <input className="create-post-input"
+      <input
+        className="create-post-input"
         type="text"
         placeholder="What's on your mind?"
         onClick={handleInputClick}
