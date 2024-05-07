70cb5b41cf417d8f1f8575a892bcb4ef1a00f0de
mendy5692 <mmm0533115692@gmail.com>
2024-05-07 12:03:35 +0300

    productPage

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>{children}</body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
