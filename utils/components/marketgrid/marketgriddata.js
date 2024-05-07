bb89920245769ac2235074fefd990c7bcd1d83ea
mendy5692 <mmm0533115692@gmail.com>
2024-05-07 15:02:49 +0300

    conect product page to the data

--- utils/components/marketgrid/marketgriddata.js
+++ utils/components/marketgrid/marketgriddata.js
@@ -1,110 +1,115 @@
+
 export const product_list = [
   {
-    title: "Green Table",
+    id: "GreenTable",
+    name: "Green Table",
     price: 500,
     location: "Haifa",
     img: "https://negativespace.co/wp-content/uploads/2024/04/negative-space-girl-in-skirt-on-golf-course-768x1152.jpg",
+    description: "puzzle ivory tuna alaska jersey bermuda bahamas highway plumber sketch harbor elephant bracelet necklace"
   },
   {
-    title: "Blue Chair",
+    id: "BlueChair",
+    name: "Blue Chair",
     price: 250,
     location: "Tel Aviv",
     img: "https://negativespace.co/wp-content/uploads/2024/04/negative-space-sunset-beach-wallpaper-1536x1023.jpg",
+    description: "chisel pixel pumpkin leaves candle tinsel snowman blizzard shovel flurry icicle boiler sleigh reindeer"
   },
   {
-    title: "Red Sofa",
+    id: "RedSofa",
+    name: "Red Sofa",
     price: 1200,
     location: "Jerusalem",
     img: "https://negativespace.co/wp-content/uploads/2024/04/negative-space-palm-trees-evening-03-944x708.jpg",
+    description: "jumper grapes pitcher gallon butcher comedy ballet script trolley subway tunnel anthem composer appliance"
   },
   {
-    title: "White Desk",
+    id: "WhiteDesk",
+    name: "White Desk",
     price: 800,
     location: "Beersheba",
     img: "https://www.shutterstock.com/shutterstock/photos/2237579873/display_1500/stock-photo-high-detail-medium-portrait-photo-a-mongolian-man-in-a-traditional-hat-looking-at-the-camera-2237579873.jpg",
+    description: "thunder lightning castle knight shepherd emperor governor dragon unicorn fairy wizard wand cloak scepter"
   },
   {
-    title: "Yellow Lamp",
+    id: "YellowLamp",
+    name: "Yellow Lamp",
     price: 120,
     location: "Eilat",
     img: "https://www.shutterstock.com/shutterstock/photos/2230331327/display_1500/stock-photo-ornate-mecha-turkey-thanksgiving-k-high-quality-render-multicolor-vivid-2230331327.jpg",
+    description: "leprechaun rainbow twister cyclone hurricane tsunami earthquake avalanche blizzard typhoon tornado volcano eruption geyser"
   },
   {
-    title: "Gray Bookshelf",
+    id: "GrayBookshelf",
+    name: "Gray Bookshelf",
     price: 450,
     location: "Netanya",
     img: "https://www.shutterstock.com/shutterstock/photos/2451944835/display_1500/stock-photo-create-a-graphic-novel-illustration-that-is-set-in-the-philippines-in-the-province-of-albay-in-2451944835.jpg",
+    description: "chocolate vanilla strawberry caramel peanut almond walnut pistachio cashew hazelnut chestnut macadamia pecan recipe"
   },
   {
-    title: "Black Coffee Table",
+    id: "BlackCoffeeTable",
+    name: "Black Coffee Table",
     price: 300,
     location: "Ashdod",
     img: "https://www.shutterstock.com/shutterstock/photos/2438857311/display_1500/stock-photo-old-school-cabover-big-rig-truck-with-slammed-suspension-on-a-highway-to-hell-with-lightning-2438857311.jpg",
+    description: "zephyr tornado maelstrom sandstorm blizzard cyclone tsunami avalanche earthquake eruption landslide mudslide hurricane typhoon monsoon"
   },
   {
-    title: "Purple Armchair",
+    id: "PurpleArmchair",
+    name: "Purple Armchair",
     price: 650,
     location: "Rishon LeZion",
     img: "https://www.shutterstock.com/shutterstock/photos/2454534503/display_1500/stock-photo-space-ships-eyeballs-aliens-planets-objects-dancers-people-animals-vehicles-swirling-in-a-2454534503.jpg",
+    description: "meteor comet asteroid nebula galaxy blackhole wormhole quasar pulsar supernova hypernova neutronstar whitehole warpspeed lightspeed"
   },
   {
-    title: "Orange Ottoman",
+    id: "OrangeOttoman",
+    name: "Orange Ottoman",
     price: 180,
     location: "Petah Tikva",
     img: "https://www.shutterstock.com/shutterstock/photos/2455784525/display_1500/stock-photo-japanese-god-man-colors-storm-cyclone-photo-realistic-wind-detailed-2455784525.jpg",
+    description: "lightning thunder rainstorm blizzard avalanche mudslide tsunami hurricane typhoon cyclone sandstorm duststorm maelstrom whirlwind vortex"
   },
   {
-    title: "Brown TV Stand",
+    id: "BrownTVStand",
+    name: "Brown TV Stand",
     price: 550,
     location: "Rehovot",
     img: "https://www.shutterstock.com/shutterstock/photos/2422989321/display_1500/stock-photo-advertising-product-photo-of-draw-me-people-during-a-puppy-yoga-activity-2422989321.jpg",
+    description: "canine feline rodent reptile amphibian arachnid insect crustacean mollusk cnidarian echinoderm sponge worm microbe virus"
   },
   {
-    title: "Ivory Dining Table",
+    id: "IvoryDiningTable",
+    name: "Ivory Dining Table",
     price: 1500,
     location: "Nahariya",
     img: "https://www.shutterstock.com/shutterstock/photos/2438948135/display_1500/stock-photo-advertising-testimonial-photo-of-houston-skyline-2438948135.jpg",
+    description: "panorama landscape scenery countryside hamlet village town city metropolis megacity township municipality burg shire vale"
   },
   {
-    title: "NavyRecliner",
+    id: "NavyRecliner",
+    name: "Navy Recliner",
     price: 700,
     location: "Karmiel",
     img: "https://www.shutterstock.com/shutterstock/photos/2341522131/display_1500/stock-photo-wet-plate-photo-of-three-men-stand-on-top-of-the-giant-stack-of-books-on-the-ocean-a-small-beer-2341522131.jpg",
+    description: "diploma degree certificate scroll parchment manuscript codex folio grimoire archive anthology compendium encyclopedia lexicon dictionary reference"
   },
   {
-    title: "Fuchsia Loveseat",
+    id: "FuchsiaLoveseat",
+    name: "Fuchsia Loveseat",
     price: 900,
     location: "Tiberias",
     img: "https://www.shutterstock.com/shutterstock/photos/2341979631/display_1500/stock-photo-create-a-photo-of-a-cappuccino-with-an-image-of-a-brain-on-the-top-2341979631.jpg",
+    description: "neuron synapse axon dendrite hippocampus amygdala cortex cerebrum cerebellum thalamus hypothalamus cognition perception emotion motivation learning"
   },
   {
-    title: "Sage Chaise Lounge",
+    id: "SageChaiseLounge",
+    name: "Sage Chaise Lounge",
     price: 1100,
-    location: "Nazareth", 
+    location: "Nazareth",
     img: "https://www.shutterstock.com/shutterstock/photos/2400550787/display_1500/stock-photo-close-up-photo-of-graphic-design-vector-bright-and-colorful-very-detailed-high-quality-a-funny-2400550787.jpg",
+    description: "surreal psychedelic fractal abstract cubist futurist avant-garde contemporary modern postmodern expressionism impressionism conceptualism minimalism maximalism"
   },
-  {
-    title: "Teal Nightstand",
-    price: 220,
-    location: "Ra'anana",
-    img: "https://www.shutterstock.com/shutterstock/photos/2293486833/display_1500/stock-photo-close-up-photo-of-lord-vishnu-sleeping-on-snake-with-laxmi-2293486833.jpg",
-  },
-  {
-    title: "Maroon Dresser",
-    price: 950,
-    location: "Beit Shemesh",
-    img: "https://www.shutterstock.com/shutterstock/photos/2336493001/display_1500/stock-photo-cyberpunk-photo-of-a-handshake-2336493001.jpg",
-  },
-  {
-    title: "Lime Stool",
-    price: 80,
-    location: "Kiryat Shmona",
-    img: "https://www.shutterstock.com/shutterstock/photos/2369301055/display_1500/stock-photo-rural-country-setting-photo-quality-s-picture-of-small-modest-farmhouse-with-small-porch-in-2369301055.jpg",
-  },
-  {
-    title: "Cherry Accent Chair", 
-    price: 450,
-    location: "Bat Yam",
-    img: "https://www.shutterstock.com/shutterstock/photos/2418655625/display_1500/stock-photo-advertising-podium-photo-of-three-fan-on-podium-cool-blue-wind-plants-and-spotlight-2418655625.jpg",
-  },
-];
+  ]
