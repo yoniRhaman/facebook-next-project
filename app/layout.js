d4cab9cabc04c8ffe2d3ae274f325d0fe53e55d3
yonatan.r <y.rahman770770@gmail.com>
2024-05-09 11:05:24 +0300

    fix cod

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/utils/components/navbar/navbar";
-import { LoadingProvider } from "@/utils/contexts/loadingContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <LoadingProvider>{children}</LoadingProvider>
+        {children}
       </body>
     </html>
   );
