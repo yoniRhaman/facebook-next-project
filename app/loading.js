bb2c664ce15e476d1dcd3fdf34b6bb0920da1f6b
yonatan.r <y.rahman770770@gmail.com>
2024-05-15 14:48:21 +0300

    fix

--- app/loading.js
+++ app/loading.js
@@ -1,12 +1,4 @@
-"use client";
-import { usePathname } from "next/navigation";
+
 export default function Loading() {
-  const pathname = usePathname();
-  return (
-    <div className="column center middleware-height">
-      {getSkeletonByPage(pathname)}
-    </div>
-  );
+  return <div className="column center middleware-height">yoni</div>;
 }
-
-
