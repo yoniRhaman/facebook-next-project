bf7b40fd5e1198e674e8c1a3ed519d0801c07bfd
yosef-tutian <yoseftutian@gmail.com>
2024-05-20 13:44:36 +0300

    api fumction

---
+++ utils/api/marketplaceApi.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:3005";
+
+async function getAllProducts() {
+  try {
+    const products = await axios.get(`${SERVER_URL}/products`);
+    return products.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
+
+async function createProduct(data) {
+  try {
+    const product = await axios.post(`${SERVER_URL}/products`, data);
+    return product.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
+
+async function deleteProduct(id) {
+  try {
+    await axios.delete(`${SERVER_URL}/products/${id}`);
+    return "Deleted";
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
