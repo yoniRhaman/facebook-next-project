4f9a37d523e8cb5b49cc9e05550eedc03d9e4b84
yosef-tutian <yoseftutian@gmail.com>
2024-05-21 15:33:38 +0300

    connect createPosts to back

---
+++ utils/api/postsApi.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:3005";
+
+export async function getAllPosts() {
+  try {
+    const posts = await axios.get(`${SERVER_URL}/posts`);
+    return posts.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
+
+export async function createNewPosts(data) {
+  try {
+    const post = await axios.post(`${SERVER_URL}/posts`, data);
+    return post.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
+
+async function deletePosts(id) {
+  try {
+    await axios.delete(`${SERVER_URL}/posts/${id}`);
+    return "DeletedPost";
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
