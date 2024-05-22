c108e8a0d09ec391be4de81738482823a1abe8de
chaimsolo <145326116+chaimsolo@users.noreply.github.com>
2024-05-22 13:43:18 +0300

    litle css changes

--- utils/components/newPostModal/newPostModal.js
+++ utils/components/newPostModal/newPostModal.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@mui/material";
 import { useState } from "react";
 import { Button, CircularProgress } from "@mui/material";
 import { Close } from "@mui/icons-material";
