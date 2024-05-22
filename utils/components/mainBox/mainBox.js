f0d25b79139a713c0cc42279baf889fbd5c0000c
yonatan.r <y.rahman770770@gmail.com>
2024-05-22 13:22:17 +0300

    make front on post page
    only left creat post

--- utils/components/mainBox/mainBox.js
+++ utils/components/mainBox/mainBox.js
@@ -2,14 +2,17 @@ import "./mainBox.css";
 import MainPosts from "./posts/mainPosts";
 import SideFreindsMenue from "./sideFreindsMenue/sideFreindsMenue";
 
-
 export default function MainBox() {
-    return (
-        <div>
-            <div className="main-box">
-                <MainPosts />
-                <SideFreindsMenue />
-            </div>
+  return (
+    <div>
+      <div className="main-box">
+        <div className="main-box-left">
+          <MainPosts />
         </div>
-    );
+        <div className="main-box-right">
+          <SideFreindsMenue />
+        </div>
+      </div>
+    </div>
+  );
 }
