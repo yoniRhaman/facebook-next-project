ae4322d646dfb6de3fa09b6a5963717505f68b6f
yonatan.r <y.rahman770770@gmail.com>
2024-05-23 13:23:04 +0300

    fixeed links local to vercal

--- app/groups/page.js
+++ app/groups/page.js
@@ -1,9 +1,11 @@
+import ChatBox from "@/utils/components/chatBox/chatBox";
 import GroupsList from "@/utils/components/groupsList/groupsList";
 
 export default function GroupsPage() {
   return (
-
+    <div className="row">
       <GroupsList />
-
+      <ChatBox />
+    </div>
   );
 }
