0d42e8e49a288f8ab33f50b973dbe1d3f92f6e66
yonatan.r <y.rahman770770@gmail.com>
2024-05-23 13:29:43 +0300

    fixe link render to vercal

--- utils/components/marketgrid/marketgrid.js
+++ utils/components/marketgrid/marketgrid.js
@@ -54,7 +54,7 @@ export default function marketgrid({ productsFromServer }) {
 export function GridItem({ product }) {
   return (
     <Link
-      href={`https://facebook-express-project.onrender.com/marketplace/product${product._id}`}
+      href={`https://facebook-next-project.vercel.app/marketplace/product${product._id}`}
       className="grid_item column"
       style={{
         // height: "30vh",
