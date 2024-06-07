c2d13ea9c690d1dac9c1fa495c4b6e8b6a1f509a
yosef-tutian <yoseftutian@gmail.com>
2024-06-07 15:46:26 +0300

    design post modal

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import MainBox from "@/utils/components/mainBox/mainBox";
-import { getCookie } from "cookies-next";
-import { cookies } from "next/headers";
+
 export default function Home() {
   return (
     <div>
       <MainBox />
-
     </div>
   );
 }
