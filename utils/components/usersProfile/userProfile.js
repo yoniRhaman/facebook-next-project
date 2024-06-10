aeb30f26b7d4e973701b8df855ebad46e8f99bda
chaimsolo <145326116+chaimsolo@users.noreply.github.com>
2024-06-10 10:59:56 +0300

    one format import deleted

--- utils/components/usersProfile/userProfile.js
+++ utils/components/usersProfile/userProfile.js
@@ -21,7 +21,6 @@ import { Button } from "@mui/material";
 import Link from "next/link";
 import { useState } from "react";
 import { addFreind } from "@/utils/api/freindsApi";
-import { format } from "date-fns";
 
 export default function UserProfile({ userData }) {
   const [isFreind, setIsFreind] = useState(userData.isFreind);
