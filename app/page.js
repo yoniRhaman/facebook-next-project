ec35e4858ce7a0015142cbf174e55e13fac71d50
mendy5692 <mmm0533115692@gmail.com>
2024-06-19 19:49:54 +0300

    TEST

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { cookies } from "next/headers";
 import MainBox from "@/utils/components/mainBox/mainBox";
 import LoginPage from "./login/page";
 
+
 export default function Home() {
   const token = getCookie("token", { cookies });
 
