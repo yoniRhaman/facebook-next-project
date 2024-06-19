e287633ca6651016d26ea5f73e92c87eb6f1b1a0
yosef-tutian <yoseftutian@gmail.com>
2024-06-19 20:47:17 +0300

    update page.js

--- app/page.js
+++ app/page.js
@@ -2,8 +2,7 @@ import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
 
 import MainBox from "@/utils/components/mainBox/mainBox";
-import LoginPage from "./login/page";
-
+import LoginPage from "./login/page.js";
 
 export default function Home() {
   const token = getCookie("token", { cookies });
