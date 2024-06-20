8cd999e5d9d53fbe2d255bf102656a98eb7b4dcf
chaimsolo <145326116+chaimsolo@users.noreply.github.com>
2024-06-20 15:32:56 +0300

    login page defined

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import MainBox from "@/utils/components/mainBox/mainBox";
 // import LoginPage from "./Login/page";
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
+import LoginPage from "./login/page";
 
 export default function Home() {
   const token = getCookie("token", { cookies });
