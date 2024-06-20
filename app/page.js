7f9be4d14a830691d6f2469d2637b528f1aac62d
chaimsolo <145326116+chaimsolo@users.noreply.github.com>
2024-06-20 15:37:14 +0300

    remove one login page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 import LoginPage from "./login/page";
 
 import MainBox from "@/utils/components/mainBox/mainBox";
-import LoginPage from "@/app/login/page.js";
+// import LoginPage from "@/app/login/page.js";
 
 export default function Home() {
   const token = getCookie("token", { cookies });
