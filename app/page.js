8a3c223f9dc84f1977f1bfa70f06f3d68489020b
yosef-tutian <yoseftutian@gmail.com>
2024-07-01 16:01:20 +0300

    remove comment

--- app/page.js
+++ app/page.js
@@ -1,15 +1,10 @@
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
 import LoginPage from "./Login/page";
-
 import MainBox from "@/utils/components/mainBox/mainBox";
-// import LoginPage from "./login/page";
-// import LoginPage from "@/app/login/page.js";
 
 export default function Home() {
   const token = getCookie("token", { cookies });
 
   return <div>{token ? <MainBox /> : <LoginPage />}</div>;
 }
-
-
